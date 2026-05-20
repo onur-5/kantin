10b2192d49349e62d436bc32d5f42adf10de51ce
Ruçhan Onur <159889208+onur-5@users.noreply.github.com>
2026-05-20 11:06:02 +0300

    Delete e-kantin directory

--- e-kantin/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://dnayzjmoeijdeflboazn.supabase.co';
-const supabaseKey = 'sb_publishable_H1SRV8ykvNX7qUGMR0iLWw_TViSSvP5';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
